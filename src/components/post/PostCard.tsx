e392e1bc6b6d2d3a7a726f04f466f4ea4eb2b5c0
Eoin Redoblado <eoin.redoblado@gmail.com>
2024-07-30 12:38:22 +1000

    padding fixes

--- src/components/post/PostCard.tsx
+++ src/components/post/PostCard.tsx
@@ -21,10 +21,10 @@ const PostCard: React.FC<PostCardProps> = ({ image, author, profileImage, title 
           <p className="text-xs">Not long ago...</p>
         </div>
       </div>
-      <div className="pl-16 pr-6 pb-6 font-bold text-xl sm:text-3xl">
+      <div className="pl-6 sm:pl-16 pr-6 pb-6 font-bold text-xl sm:text-3xl">
         {title}
       </div>
-      <div className="pl-16 pr-6 pb-6">
+      <div className="pl-6 sm:pl-16 pr-6 pb-6">
         Lorem ipsum odor amet, consectetuer adipiscing elit. Leo auctor orci primis, sollicitudin quisque consectetur. Nec facilisi dolor netus tempus sodales aliquam sodales? Ante tristique et integer suspendisse aliquet interdum lobortis. Aenean primis per lobortis cursus at hendrerit sed fermentum pretium.
       </div>
     </div>
