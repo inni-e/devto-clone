aa8cb054e4b0de5448f7c29ca674cad2b6c1eb01
Eoin Redoblado <eoin.redoblado@gmail.com>
2024-08-03 20:48:29 +1000

    img tag

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const ProfileImage = (props: { image: string }) => {
   return (
-    <Image src={props.image} alt="profile image" width={32} height={32} className="rounded-full" />
+    <img src={props.image} alt="profile image" width={32} height={32} className="rounded-full" />
   );
 }
 
