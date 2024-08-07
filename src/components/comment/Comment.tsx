466b39f0b03aaca57aa0860faa1c49370900dd74
Eoin Redoblado <eoin.redoblado@gmail.com>
2024-08-08 01:18:57 +1000

    completed comments

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import { api } from '~/utils/api';
 import { User } from '@prisma/client';
+import { useSession } from 'next-auth/react';
+
+import Link from "next/link";
+import ProfileImage from "~/components/ProfileImage";
+import LoadingSpinner from '../LoadingSpinner';
 
 interface CommentType {
   id: number;
@@ -25,9 +30,10 @@ interface CommentProps {
 
 export const Comment = ({ comment }: CommentProps) => {
   const [reply, setReply] = useState('');
-  const replyMutation = api.comment.replyToComment.useMutation();
+  const [isReplying, setisReplying] = useState(false);
+  const { mutate: replyToComment, isPending: isReplyingToComment } = api.comment.replyToComment.useMutation();
   const handleReply = () => {
-    replyMutation.mutate({ commentId: comment.id, content: reply });
+    replyToComment({ commentId: comment.id, content: reply });
     setReply('');
   };
 
@@ -35,20 +41,72 @@ export const Comment = ({ comment }: CommentProps) => {
   const { data: replies, isLoading } = api.comment.getRepliesByComment.useQuery({ commentId: comment.id });
 
   return (
-    <div className="border p-2">
-      <p>{comment.user.name}: {comment.content}</p>
+    <div className="flex flex-col p-2 mt-2">
+      <div className='flex flex-row justify-start gap-2'>
+        <Link href={"/user/" + comment.user.id}>
+          <ProfileImage
+            className="w-8 h-8"
+            user={{
+              ...comment.user,
+              name: comment.user.name ?? null,
+              image: comment.user.image ?? null,
+              email: comment.user.email ?? null,
+            }}
+          />
+        </Link>
+        <div className='grow flex flex-col gap-2'>
+          <div className='p-4 grow border border-gray-200 rounded-md'>
+            <span className='text-md font-bold'>{comment.user.name}</span>
+            <span className='ml-2 text-sm text-gray-400'>Yesterday</span>
+            <p className='mt-4'>
+              {comment.content}
+            </p>
+          </div>
+          <div className='flex justify-start gap-2'>
+            <button className='flex justify-between items-center gap-1 text-sm px-2 py-1 rounded-md hover:bg-gray-100'>
+              <svg width="24" height="24"><title id="a5t94onskymmyvtvo92b9mlqetvdk61p">Like comment: </title><path d="M18.884 12.595l.01.011L12 19.5l-6.894-6.894.01-.01A4.875 4.875 0 0112 5.73a4.875 4.875 0 016.884 6.865zM6.431 7.037a3.375 3.375 0 000 4.773L12 17.38l5.569-5.569a3.375 3.375 0 10-4.773-4.773L9.613 10.22l-1.06-1.062 2.371-2.372a3.375 3.375 0 00-4.492.25v.001z"></path></svg>
+              6 likes
+            </button>
+            <button
+              className='flex justify-between items-center gap-1 text-sm px-2 py-1 rounded-md hover:bg-gray-100'
+              onClick={() => setisReplying(!isReplying)}
+            >
+              Reply
+            </button>
+          </div>
+          {isReplying &&
+            <>
+              <textarea
+                value={reply}
+                onChange={(e) => setReply(e.target.value)}
+                placeholder="Reply to this comment"
+                style={{
+                  resize: 'none', // Prevent resizing
+                }}
+                className="border p-2 h-16 grow rounded-md focus:outline-blue-700 focus:h-[9rem]"
+              />
+              <button
+                onClick={handleReply}
+                className="bg-blue-700 flex justify-center items-center font-bold text-white p-1 w-16 h-9 rounded-md hover:bg-blue-800"
+                disabled={isReplyingToComment}
+              >
+                {isReplyingToComment ?
+                  <LoadingSpinner />
+                  :
+                  "Reply"
+                }
+
+              </button>
+            </>
+          }
+        </div>
+      </div>
       <div className="ml-4">
+
         {replies?.map((reply) => (
           <Comment key={reply.id} comment={reply} />
         ))}
-        <input
-          type="text"
-          value={reply}
-          onChange={(e) => setReply(e.target.value)}
-          placeholder="Reply to this comment"
-          className="border p-1"
-        />
-        <button onClick={handleReply} className="ml-2 bg-blue-500 text-white p-1">Reply</button>
+
       </div>
     </div>
   );
@@ -61,17 +119,55 @@ type PostIdProps = {
 export const CommentsSection = ({ postId }: PostIdProps) => {
   const { data: comments, isLoading } = api.comment.getCommentsByPost.useQuery({ postId });
   const [newComment, setNewComment] = useState('');
-  const createCommentMutation = api.comment.createComment.useMutation();
+  const { mutate: createComment, isPending: isCommenting } = api.comment.createComment.useMutation();
+  const { data: sessionData } = useSession();
 
   const handleComment = () => {
-    createCommentMutation.mutate({ postId, content: newComment });
+    createComment({ postId, content: newComment });
     setNewComment('');
   };
 
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div>
+    <div className='border-t border-gray-200 w-full py-8 px-16'>
+      <h1 className='font-bold text-3xl mb-8'>Comments</h1>
+      {sessionData &&
+        <div className='flex justify-start gap-2 mb-8'>
+          <Link href={"/user/" + sessionData.user.id}>
+            <ProfileImage
+              className="w-8 h-8"
+              user={{
+                ...sessionData.user,
+                name: sessionData.user.name ?? null,
+                image: sessionData.user.image ?? null,
+                email: sessionData.user.email ?? null,
+              }}
+            />
+          </Link>
+          <div className='flex flex-col grow gap-4'>
+            <textarea
+              value={newComment}
+              onChange={(e) => setNewComment(e.target.value)}
+              placeholder="Add to the discussion"
+              style={{
+                resize: 'none', // Prevent resizing
+              }}
+              className="border p-2 h-16 grow rounded-md focus:outline-blue-700 focus:h-[9rem]"
+            />
+            <button
+              onClick={handleComment}
+              className="bg-blue-700 font-bold text-white p-1 w-20 h-9 rounded-md hover:bg-blue-800"
+            >
+              {isCommenting ?
+                <LoadingSpinner />
+                :
+                "Submit"
+              }
+            </button>
+          </div>
+        </div>
+      }
       {comments?.map((comment) => (
         <Comment key={comment.id} comment={{
           id: comment.id,
@@ -82,14 +178,7 @@ export const CommentsSection = ({ postId }: PostIdProps) => {
           replies: comment.replies
         }} />
       ))}
-      <input
-        type="text"
-        value={newComment}
-        onChange={(e) => setNewComment(e.target.value)}
-        placeholder="Add a comment"
-        className="border p-1"
-      />
-      <button onClick={handleComment} className="ml-2 bg-blue-500 text-white p-1">Comment</button>
+
     </div>
   );
 };
--- src/components/post_creation/CreatePostView.tsx
+++ src/components/post_creation/CreatePostView.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import AutoResizeTextArea from "~/components/post_creation/AutoResizeTextArea";
 // import FileUpload from "~/components/post_creation/FileUpload"; // Import your file upload component
 import { api } from "~/utils/api";
+import LoadingSpinner from '~/components/LoadingSpinner';
 
 // TODO: Uninstall axios
 
@@ -106,10 +107,7 @@ export default function CreatePostView() {
         <div className="flex items-center justify-start gap-4 py-6">
           {isPosting || isGettingURL ?
             <div className="text-white font-bold w-24 h-10 bg-blue-700 hover:bg-blue-800 rounded-md flex justify-center items-center">
-              <svg className="animate-spin h-5 w-5 text-white" viewBox="0 0 24 24">
-                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-              </svg>
+              <LoadingSpinner />
             </div>
             :
             <button
